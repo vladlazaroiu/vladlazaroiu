7e4b8418dd14b213d075e22784fe3c1844c1fb31
sucacu <ekro98@gmail.com>
2017-10-28 17:33:54 +0300

    Sucacu's patch 1 (#7)
    * Test commit by Antonio
    
    * Create NewFileP01.cpp
    
    Fisier nou necesar!

---
+++ NewFileP01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void T1(){
+	cout << "Test method, number 1";
+}
+
+int main(){
+	cout << "There are multiple methods called, they are:\n";
+	T1();
+}
--- TestCode.c
+++ TestCode.c
@@ -2,6 +2,7 @@
 
 int main(){
 	int n;
+	scanf("%d",&n);
 	scanf("%d", &n);
 	printf("%d", n << 10);
 	return 0;
